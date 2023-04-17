cc59df42033eefd98923aa1fd6acbab9521aad46
Peeranat Phengsalaeh <seed.peeranat@gmail.com>
2023-04-17 22:24:26 +0700

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./assets/styles/App.css";
+import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+const LoginPage = () => {
+  return <h1>Login</h1>;
+};
+
+export default LoginPage;
