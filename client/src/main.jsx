b47047f94383606dbec40f24fac206f604636df2
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-04-20 14:48:09 +0700

    feature-praew-registerpage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import "./assets/styles/index.css";
 import "./assets/styles/login.css";
 import { BrowserRouter } from "react-router-dom";
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
