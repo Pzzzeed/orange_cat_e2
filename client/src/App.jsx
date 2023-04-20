01dd35ae7ca923b968cd51ddd1e35b51cc29d53e
kittipol <kittipol.surapinchai@gmail.com>
2023-04-20 10:53:25 +0700

    feat:loginpage without navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./assets/styles/App.css";
+import "./assets/styles/login.css";
 import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,70 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+import {faKey} from '@fortawesome/free-solid-svg-icons'
+import { FaGoogle } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook} from "react-icons/fa";
+import "../assets/styles/login.css"
+// import usePasswordToggle from "../../Hooks/usePasswordToggle";
+
+
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.min.js";
 
 const LoginPage = () => {
-  return <h1>Login</h1>;
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+  return (
+    <section>
+    <div className="form-box">
+      <div className="form-value">
+        <form action="">
+          <h2>Login</h2>
+          <div className="inputbox">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <input type="email" placeholder="Email" required="required" />
+          </div>
+          <div className="inputbox">
+          <FontAwesomeIcon icon={faKey} />
+            <input type="password" placeholder="Password" required="required" />
+          </div>
+          <div className="forget">
+            <label htmlFor="">
+              <input type="checkbox" />Remember Me<a href="#">Forget Password</a>
+            </label>
+          </div>
+          <button>Sign in</button>
+          <div className="text-con">
+              <p>or continue with</p>
+          </div>
+        </form>
+        <div className="social">
+          <div className="social1">
+           <a href=""><FaGoogle /></a>
+          </div>
+          <div className="social2">
+           <a href=""><FaInstagram /></a>
+          </div>
+          <div className="social3">
+           <a href=""><FaFacebook/></a>
+          </div>
+        </div>
+        <div className="footer">
+          <p>Dont have an account yet?</p>
+          <a href="">Register for free</a>
+        </div>
+      </div>
+    </div>
+    <div className="body-cat">
+        <img src="./Body Cat1.png" alt="" />
+    </div>
+    </section>
+  )
 };
 
 export default LoginPage;
