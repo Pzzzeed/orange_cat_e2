e76a258bf8f9d045db9272bd479a0bc031583684
Haluethaichanok-J <haluethaichanok@gmail.com>
2023-04-21 19:07:38 +0700

    feature-praew-navbar-after-login

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import '../assets/styles/NavBar.css'
 const NavBar = () => {
-  return(
+const [navAfter,setNavAfter] = useState(false)
 
-    <nav className="nav">
+const toggle = () =>{
+var toggleNav = document.getElementById('nav-menu')
+if(toggleNav.style.display === 'none'){
+  toggleNav.style.display = 'inline'
+}
+else{
+  toggleNav.style.display = 'none'
+}
+}
+
+  return(
+<nav>
+{/* before login navbar */}
+    <div className="nav" >
      
 <a className="site-title"><img id='nav-img' src="/head-logo.png"/>Orange Cat</a>
-
-      
-      
         <ul className="nav-middle">
           <li><a href="/">Feature</a></li>
           <li><a href="/">BMI</a></li>
           <li><a href="/">How It Work</a></li>
-        </ul>
-    
-   
+        </ul> 
 <ul className="nav-right">
 <li><a href="/login">Sign in</a></li>
 <li><a href="/register">Register</a></li>
 </ul>
-    </nav>
+    </div>
+
+
+{/* After login navbar */}
+<div className="navAfter" style={{display:'none'}}>
+<a className="site-title"><img id='nav-img' src="/head-logo.png"/>Orange Cat</a>
+        <ul className="nav-middle">
+          <li><a href="/">Feature</a></li>
+          <li><a href="/">BMI</a></li>
+          <li><a href="/">How It Work</a></li>
+        </ul> 
+<div className="allNavRight">
+<div className="nav-right">
+<img id="profilepic" src="/public/Login_logo.png"/>
+<button onClick={toggle} id="nav-toggle">Username</button>
+</div>
+
+<div id="nav-menu" style = {{display:'none'}}>
+<ul className="nav-links">
+<li className="nav-item"><a href="#">Profile</a></li>
+  <li className="nav-item"><a href="#">Profile</a></li>
+  <li className="nav-item"><a href="#">Profile</a></li>
+
+</ul>
+</div>
+    </div>
+</div>
+</nav>
   )
 };
 
